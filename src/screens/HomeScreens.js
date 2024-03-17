1f0855eb0ce05e7b36046ddb7e9781f3d4e231bf
viveksinra <vivekkrvks@gmail.com>
2024-03-17 14:10:28 +0530

    fix

--- src/screens/HomeScreens.js
+++ src/screens/HomeScreens.js
@@ -1,42 +1,55 @@
-import React, { useState, useContext, useRef } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import { View, Text, TextInput, Button, Alert, StyleSheet,Animated,ScrollView,  ImageBackground } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import Corn from '../components/Corn';
 
-import ContentContext from '../Context/ContentContext';
+import ContentContext, { startUrl } from '../Context/ContentContext';
 const image = { uri: ContentContext.mainBg };
 import { useNavigation } from '@react-navigation/native';
 import TermPopup from '../authentication/authComponent/MobileLogin/TermPopup';
 import ListProduct from '../components/Product/ListProduct';
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 
 
 const HomeScreens = () => {
   const navigation = useNavigation();
   const { t } = useTranslation();
  const [allProduct, setAllProduct] = useState([
-  {_id:"absd",productName:"Maize (G1 Quality)", quality:"High-Quality Grade 1 Maize",price:"1850",productImage:"https://m.media-amazon.com/images/I/61V1TTqTu6L._AC_UF1000,1000_QL80_.jpg"},
-  {_id:"bsd",productName:"Maize (G2 Quality)", quality:"Quality Grade 2 Maize",price:"1800",productImage:"https://seed2plant.in/cdn/shop/products/maizeseeds.jpg?v=1604034397"},
-  {_id:"sd",productName:"Maize (G3 Quality)", quality:"Quality Grade 3 Maize",price:"1710",productImage:"https://5.imimg.com/data5/MU/QR/MY-12687905/best-quality-yellow-maize.jpg"},
+  {_id:"absd",productName:"Test (Server Error)", quality:"High-Quality Grade 1 Maize",price:"9950",productImage:"https://static.doofinder.com/main-files/uploads/2019/09/error-500-doofinder.jpg"},
+  {_id:"bsd",productName:"Test (Server Error)", quality:"Quality Grade 2 Maize",price:"9600",productImage:"https://static.doofinder.com/main-files/uploads/2019/09/error-500-doofinder.jpg"},
+  {_id:"sd",productName:"Test (Server Error)", quality:"Quality Grade 3 Maize",price:"9510",productImage:"https://static.doofinder.com/main-files/uploads/2019/09/error-500-doofinder.jpg"},
  ])
 
- const getProduct = async() => {
+ const getProduct = async () => {
   const url = `${startUrl}/chattiApi/allCommon/product/getProductForPublic`;
 
- // Retrieve the authorization token from SecureStore
- const token = await SecureStore.getItemAsync('authToken');
+  // Retrieve the authorization token from SecureStore
+  const token = await SecureStore.getItemAsync('authToken');
 
- // Make an API call to get the chat count from the server
- const response = await axios.get(url, {
-   headers: {
-     Authorization: token,
-   },
- });
- if(response.data.variant == "success"){ 
-   apiChatCount = response.data.count;
- }else{
-  alert("Failed to Connect to server, check your internet connection")
- }
-}
+  try {
+    // Make an API call to get the chat count from the server
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    if (response.data.variant === "success") {
+      const responseData = response.data; // Assuming products is the array you want
+      setAllProduct(responseData.data);
+    } else {
+      alert("Failed to Connect to server, check your internet connection");
+    }
+  } catch (error) {
+    console.error("Error fetching product data:", error);
+    alert("Failed to fetch product data");
+  }
+};
+
+useEffect(() => {
+  getProduct();
+}, []);
+
 
   return (
 <ScrollView>
