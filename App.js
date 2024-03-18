edf109fa2eedb19090021c69d028bff16967976b
viveksinra <vivekkrvks@gmail.com>
2024-03-18 15:31:55 +0530

    app is working now

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './src/navigation';
 import { AppContextProvider } from './context/appContext';
+import 'react-native-gesture-handler';
+
 import i18n from './src/lan/i18n'
 export default function App() {
   return (
